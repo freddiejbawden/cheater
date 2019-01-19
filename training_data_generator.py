65344e40672e15fe5dc4afc64f886d018df53165
KieranLitschel <kieran.litschel@hotmail.co.uk>
2019-01-19 14:10:00 +0000

    Finished training_data_generator

--- training_data_generator.py
+++ training_data_generator.py
@@ -1,9 +1,11 @@
 import cv2
+import time
+import datetime
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():  # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
@@ -12,6 +14,17 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')
+    if key == 13:
+        key = cv2.waitKey()
+        if key == 43:
+            print("Truth")
+            cv2.imwrite("Truth/" + st + ".jpg", frame)
+        if key == 45:
+            print("Lie")
+            cv2.imwrite("Lie/" + st + ".jpg", frame)
+
+    if key == 27:  # exit on ESC
         break
 cv2.destroyWindow("preview")
