bba875cb9c0c3870e9d8e8a219e39102c1b53ee6
KieranLitschel <kieran.litschel@hotmail.co.uk>
2019-01-19 15:43:55 +0000

    Integrated Microsoft Azure Face API

--- training_data_generator.py
+++ training_data_generator.py
@@ -1,6 +1,28 @@
 import cv2
 import time
 import datetime
+import requests
+
+f_api = open("api_code.txt", "r")
+api_key = f_api.readline()
+
+
+def pred_image(frame):
+    headers = {'Ocp-Apim-Subscription-Key': api_key, 'Content-Type': 'application/octet-stream'}
+
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'smile,emotion',
+    }
+
+    response = requests.post(face_api_url, params=params, headers=headers, data=frame)
+    jsonResponse = response.json()
+
+    return jsonResponse[0]["faceId"], jsonResponse[0]["faceAttributes"]
+
+
+face_api_url = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect'
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
@@ -13,17 +35,22 @@ else:
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
-    key = cv2.waitKey(20)
+    key = cv2.waitKey(333)
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')
     if key == 13:
+        frame_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
         key = cv2.waitKey()
         if key == 43:
             print("Truth")
             cv2.imwrite("Truth/" + st + ".jpg", frame)
+            _, face_atts = pred_image(frame_bytes)
+            print(face_atts)
         if key == 45:
             print("Lie")
             cv2.imwrite("Lie/" + st + ".jpg", frame)
+            _, face_atts = pred_image(frame_bytes)
+            print(face_atts)
 
     if key == 27:  # exit on ESC
         break
