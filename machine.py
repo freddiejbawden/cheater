1e9fc7c554c3736447a1e32fe07c34d34d190361
KieranLitschel <kieran.litschel@hotmail.co.uk>
2019-01-20 08:09:45 +0000

    Added random chance of best move becoming other move with same score

--- machine.py
+++ machine.py
@@ -1,9 +1,10 @@
 from itertools import combinations
+import random
 
 VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 # We assume the probability of someone calling cheat when you lie is 0.1, this is somewhat optimistic, but could
 # be adjusted at a later date
-prob_call_cheat = 0.9
+prob_call_cheat = 0.5
 
 
 def possible_moves(last_move):
@@ -76,19 +77,25 @@ class AIPlayer:
 
     def other_players_play(self, played_value, number_played):
         self.cards_in_pile += number_played
-        if self.our_hand[played_value] - number_played < 0:
+        if self.our_hand[played_value] + number_played > 4:
+            self.last_played_value = None
             return "CHEAT"
         else:
             return self.predict_other_player(played_value)
 
     def predict_other_player(self, played_value):
         # Add predicting emotions
-        self.last_played = played_value
+        self.last_played_value = played_value
         return "TRUTH"
 
     def play_cards(self):
-        if self.last_played is None:
-            print("Something")
+        _, best_move = self.next_best_move(self.last_played_value, self.our_hand, 0, 2)
+        for card in best_move[2:len(best_move)]:
+            self.our_hand[card] -= 1
+        if len(best_move) < 3:
+            print("Error got best_move is %s" % best_move)
+        else:
+            print("Say: %s * %ss, Play: %s" % (best_move[0], best_move[1], best_move[2:len(best_move)]))
 
     def next_best_move(self, last_played_value, cards_in_hand, score_to_date, depth):
         if depth == 0:
@@ -108,9 +115,9 @@ class AIPlayer:
                     # could be adjusted at a later date
                     other_player_poss_values_probs[other_player_poss_value] = 1 / len(other_player_poss_values)
                 for no_cards_played in range(2, 5):
-                    new_score = score_to_date + no_cards_played
                     for cards_played in possible_cards_to_add_to_pile(cards_in_hand,
                                                                       no_cards_played):  # -2 as noCardsPlayed starts at 2, but is indexed at 0
+                        new_score = score_to_date + no_cards_played
                         new_cards_in_hand = cards_in_hand.copy()
                         for card_played in cards_played:
                             new_cards_in_hand[card_played] -= 1
@@ -134,10 +141,13 @@ class AIPlayer:
                                                                depth - 1)
                                 poss_future_scores += score * other_player_poss_values_probs[other_player_next_value]
                             new_score += poss_future_scores
-                        #print("%s, %s" % (new_score, "Say: " + str(no_cards_played) + " * " + value + ", Play: " +
+
+                        # print("%s, %s" % (new_score, "Say: " + str(no_cards_played) + " * " + value + ", Play: " +
                         #                 ",".join(cards_played)))
-                        if new_score > best_score:
+                        
+                        # Add a random chance of the best move being set to a different value even if the score will
+                        # be the same, this makes it slightly harder to see through the AI's lies
+                        if new_score > best_score or (new_score == best_score and random.random() >= 0.5):
                             best_score = new_score
-                            best_move = "Say: " + str(no_cards_played) + " * " + value + ", Play: " + ",".join(
-                                cards_played)
+                            best_move = [str(no_cards_played), value] + list(cards_played)
             return best_score, best_move
